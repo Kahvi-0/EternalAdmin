df34747c9994736b18fe3b0487357d1d2067b25f
Kahvi-0xFF <46513413+Kahvi-0@users.noreply.github.com>
2023-01-29 21:16:38 -0500

    Update EternalAdmin.py

--- EternalAdmin.py
+++ EternalAdmin.py
@@ -13,6 +13,31 @@ PASSWORD = ''
 newuser = ''
 newpwd = ''
 
+#Notes from original 
+'''
+Note:
+- The exploit should never crash a target (chance should be nearly 0%)
+- The exploit use the bug same as eternalromance and eternalsynergy, so named pipe is needed
+
+Tested on:
+- Windows 2016 x64
+- Windows 10 Pro Build 10240 x64
+- Windows 2012 R2 x64
+- Windows 8.1 x64
+- Windows 2008 R2 SP1 x64
+- Windows 7 SP1 x64
+- Windows 2008 SP1 x64
+- Windows 2003 R2 SP2 x64
+- Windows XP SP2 x64
+- Windows 8.1 x86
+- Windows 7 SP1 x86
+- Windows 2008 SP1 x86
+- Windows 2003 SP2 x86
+- Windows XP SP3 x86
+- Windows 2000 SP4 x86
+'''
+
+
 ###########################
 # info for modify session security context
 ###########################
